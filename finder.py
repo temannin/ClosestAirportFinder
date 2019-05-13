de4f9c386fca14064497da674538aa8c8e4c84b0
Tyler Manning <tylerericmanning@gmail.com>
2019-05-13 00:03:22 -0400

    Formatted

--- finder.py
+++ finder.py
@@ -3,10 +3,11 @@ import re
 
 from math import sin, cos, sqrt, atan2, radians
 
+
 def main():
 
-    LAT_ORIGIN = radians(39.103119) # YOUR LOCATION LATITUDE IN ( )
-    LON_ORIGIN = radians(-84.512016) # YOUR LOCATION LONGITUDE IN ( )
+    LAT_ORIGIN = radians(39.103119)  # YOUR LOCATION LATITUDE IN ( )
+    LON_ORIGIN = radians(-84.512016)  # YOUR LOCATION LONGITUDE IN ( )
 
     radius_of_earth = 6378.0
 
@@ -16,12 +17,12 @@ def main():
         with open('airports.json') as json_file:
             data = json.load(json_file)
             for line in airports:
-                if line.strip():                    
+                if line.strip():
                     regex = r"\((.*)\)"
                     matches = re.search(regex, line)
                     if matches:
-                        DEST = "K"  + matches.group(1)
-                
+                        DEST = "K" + matches.group(1)
+
                         for airport in data:
                             if DEST == airport:
                                 lat2 = radians(data[airport]["lat"])
@@ -30,11 +31,13 @@ def main():
                                 dlon = lon2 - LON_ORIGIN
                                 dlat = lat2 - LAT_ORIGIN
 
-                                a = sin(dlat / 2)**2 + cos(LAT_ORIGIN) * cos(lat2) * sin(dlon / 2)**2
+                                a = sin(dlat / 2)**2 + cos(LAT_ORIGIN) * \
+                                    cos(lat2) * sin(dlon / 2)**2
                                 c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
-                                distance = radius_of_earth * c * .621371 # Remove .621371 for distance in kilometers.
-                                
+                                # Remove .621371 for distance in kilometers.
+                                distance = radius_of_earth * c * .621371
+
                                 result = {
                                     "name": data[airport]["name"],
                                     "distance": distance
@@ -43,9 +46,10 @@ def main():
                                 results.append(result)
 
         results = sorted(results, key=lambda k: k['distance'])
-    
+
     for result in results:
         print(result)
+
 
 if __name__ == "__main__":
     main()
